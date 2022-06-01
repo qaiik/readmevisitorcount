11c27ea79e697bffcc1e143e69853f14d8f109c6
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-01 00:22:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const app = require("express")();
 const fs = require("fs");
 app.get("/", (req,res)=>{
+    console.log("hi")
     let n = fs.readFileSync("./vis.txt").toString()
     fs.writeFileSync("./vis.txt", Number(n)+1)
     let visitor = fs.readFileSync("./vis.txt").toString()
     res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control","no-cache, no-store")
+  res.setHeader("Expires", "Mon, 01 Jan 1990 00:00:00 GMT")
+  res.setHeader("Last-Modified", "Mon, 01 Jan 2999 00:00:00 GMT")
+  res.setHeader("id", String(Math.random()))
     res.send(`<svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
   <style>
     .small { font: italic 13px sans-serif; fill: white; }
