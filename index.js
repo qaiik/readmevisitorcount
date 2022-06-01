c7ee4e50802cf45d642744ab4f66980f33cd91aa
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-01 00:13:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const fs = require("fs");
 app.get("/", (req,res)=>{
     let n = fs.readFileSync("./vis.txt").toString()
-    fs.writeFileSync("./vis.txt", Number(n))
+    fs.writeFileSync("./vis.txt", Number(n)+1)
     let visitor = fs.readFileSync("./vis.txt").toString()
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`<svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
